65ccc1a6dc6c144b5b1371b2b64a14facf7fff8c
marina <marinamarrina@hotmail.com>
2021-11-03 15:37:16 +0100

    validation form registro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,16 +10,16 @@
                         {{ __('Register') }}
                     </div>
     
-                        <form class="py-10 px-5" method="POST" action="{{ route('register') }}">
+                        <form class="py-10 px-5" method="POST" action="{{ route('register') }}" novalidate>
                             @csrf
     
                             <div class="flex flex-wrap mb-6">
                                 <label for="name" class="block text-gray-700 text-sm mb-2">{{ __('Name') }}</label>
     
-                                    <input id="name" type="text" class="p-3 bg-gray-200 rounded from-input w-full @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                    <input id="name" type="text" class="p-3 bg-gray-200 rounded from-input w-full @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" autocomplete="name" autofocus>
     
                                     @error('name')
-                                        <span class="invalid-feedback" role="alert">
+                                    <span class="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 w-full mt-5 text-sm" role="alert">
                                             <strong>{{ $message }}</strong>
                                         </span>
                                     @enderror
@@ -28,10 +28,10 @@
                             <div class="flex flex-wrap mb-6">
                                 <label for="email" class="block text-gray-700 text-sm mb-2">{{ __('E-Mail Address') }}</label>
     
-                                    <input id="email" type="email" class="p-3 bg-gray-200 rounded from-input w-full @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                    <input id="email" type="email" class="p-3 bg-gray-200 rounded from-input w-full @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" autocomplete="email">
 
                                     @error('email')
-                                        <span class="invalid-feedback" role="alert">
+                                    <span class="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 w-full mt-5 text-sm" role="alert">
                                             <strong>{{ $message }}</strong>
                                         </span>
                                     @enderror                               
@@ -40,10 +40,10 @@
                             <div class="flex flex-wrap mb-6">
                                 <label for="password" class="block text-gray-700 text-sm mb-2">{{ __('Password') }}</label>
     
-                                <input id="password" type="password" class="p-3 bg-gray-200 rounded from-input w-full @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <input id="password" type="password" class="p-3 bg-gray-200 rounded from-input w-full @error('password') is-invalid @enderror" name="password" autocomplete="new-password">
     
                                     @error('password')
-                                        <span class="invalid-feedback" role="alert">
+                                    <span class="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 w-full mt-5 text-sm" role="alert">
                                             <strong>{{ $message }}</strong>
                                         </span>
                                     @enderror                               
@@ -51,7 +51,7 @@
     
                             <div class="flex flex-wrap mb-6">
                                 <label for="password-confirm" class="block text-gray-700 text-sm mb-2">{{ __('Confirm Password') }}</label>                                
-                                <input id="password-confirm" type="password" class="p-3 bg-gray-200 rounded from-input w-full" name="password_confirmation" required autocomplete="new-password">                                
+                                <input id="password-confirm" type="password" class="p-3 bg-gray-200 rounded from-input w-full" name="password_confirmation" autocomplete="new-password">                                
                             </div>
     
                             <div class="flex flex-wrap">               
