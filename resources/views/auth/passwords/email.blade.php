c4fb79a9cd5b013b856dbe3dfc45e0bfa7160bd6
marina <marinamarrina@hotmail.com>
2021-11-03 20:00:31 +0100

    altri

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -36,7 +36,7 @@
                                 </button>                            
                         </div>
                     </form>
-                </div>
+               
             </div>
         </div>
     </div>
