66c9cbc192159a5e261a233254b8f79b20df2332
marina <marinamarrina@hotmail.com>
2021-11-10 21:07:45 +0100

    altri

--- app/Vacante.php
+++ app/Vacante.php
@@ -10,4 +10,10 @@ class Vacante extends Model
     protected $fillable = [
         'titulo', 'imagen', 'descripcion', 'skills', 'categoria_id', 'experiencia_id', 'ubicacion_id', 'salario_id'
     ];
+
+    //RELACION 1:1 CATEGORIA Y VACANTE
+    public function categoria()
+    {
+        return $this->belongsTo(Categoria::class);
+    }
 }
